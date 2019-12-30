636186f370cbe6ef9acc778e44e598aa7c60af24
Tácio <maxantonio852@gmail.com>
2019-12-30 00:43:00 -0300

    Correction in src/Main

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-first-prop-new-line */
+/* eslint-disable arrow-body-style */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
